44ca8923ab664c58c139a92fefb17ca36001a931
makubexd <makubex1719@gmail.com>
2017-08-17 00:11:14 -0500

    filters

---
+++ AngularFacilito/app/filterController.js
@@ -0,0 +1,13 @@
+﻿angular.module("filterModule", [])
+    .filter("removeHtml", function () {
+        return function (textHtml) {
+            return String(textHtml).replace(/<[^>]+>/gm, '');
+        }
+    })
+    .controller("filterController", function ($scope) {
+        $scope.htmlTag = "<p>Here is a part of HTML</p>";
+        $scope.my_Html = {};
+        $scope.my_Html.title = "TitleHtml";
+        $scope.my_Html.body = "bodyHtml";
+        $scope.cost = 2;
+    })
